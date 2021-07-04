f252811f67a7a94f4fbd91578afc13e6babc5bc0
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-07-04 18:44:08 +0100

    Add(script): function displayAdditionQuestion

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }  
         })
     }
+
+    runGame("addition");
 })
 
-function runGame() {
+function runGame(gameType) {
 
     // Generate two random numbers between 1 and 25
     // Math.floor rounds down to the whole number
@@ -24,6 +26,13 @@ function runGame() {
 
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 + 1);
+
+    if (gameType ==="addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game tyoe ${gameType}, aborting`;
+    }
 }
 
 function checkAnswer() {
@@ -46,9 +55,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
-
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+    
 }
 
 function displaySubstractQuestion() {
