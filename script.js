959c40502b712004d07723352f71760044d3c2a1
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-07-04 23:54:52 +0100

    Add(script): function displayMultiplyQuestion

--- script.js
+++ script.js
@@ -27,8 +27,10 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 + 1);
 
-    if (gameType ==="addition") {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type ${gameType}`);
         throw `Unknown game tyoe ${gameType}, aborting`;
@@ -67,6 +69,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, aborting`;
@@ -105,7 +109,10 @@ function displaySubstractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
 
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 
 }
